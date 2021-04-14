b589299a48c9a3e0de9ecd95f189acc9b67c0e5b
Girija Shankar Swain <girijashankar.swain@rediffmail.com>
2021-04-14 11:45:04 +1000

    upgrades to latest spring boot version

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import com.gsswain.file.DemoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration(classes=DemoApplication.class)
 public class DemoApplicationTests {
 
 	@Test
